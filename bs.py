8a4993e04504232c19a851b343ebe1346a4dbf61
DevZro <danizhem99@gmail.com>
2026-07-19 17:47:43 +0100

    Binary Search Added

---
+++ bs.py
@@ -0,0 +1,15 @@
+# Binary Search
+
+def search(nums, target):
+    l = 0
+    r = len(nums) - 1
+
+    while l <= r:
+        cur = (l + r) // 2
+        if nums[cur] < target:
+            l = cur + 1
+        elif nums[cur] > target:
+            r = cur - 1
+        else: 
+            return cur
+    return -1 
