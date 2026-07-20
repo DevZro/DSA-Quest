911e79ccdd1f24bc5b93d0698bede4acbd53d19f
DevZro <danizhem99@gmail.com>
2026-07-20 09:25:45 +0100

    Koko Eating Bananas

---
+++ keb.py
@@ -0,0 +1,32 @@
+# Koko Eating Bananas 
+
+import math
+
+def minEatingSpeed(self, piles: List[int], h: int) -> int:
+    m = max(piles)
+    if len(piles) == h:
+        return m
+        
+    s = sum(piles)
+    n = len(piles)
+
+    lower = math.ceil(s / h)
+    upper = min(math.ceil((s - n) / (h - n)), m)
+
+    def check(r):
+        val = 0
+        for pile in piles:
+            val += math.ceil(pile / r)
+            if val > h:
+                return False
+        return True
+        
+    while lower < upper:
+        mid = lower + (upper - lower) // 2
+
+        if check(mid):
+            upper = mid
+        else:
+            lower = mid + 1
+        
+    return lower
